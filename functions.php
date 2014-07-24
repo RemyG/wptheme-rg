bce64ea34a8698a8e02d240d88a494c05560cf64
RemyG <remy.gardette@gmail.com>
2014-07-24 15:00:23 +0100

    Added body background and content style

--- functions.php
+++ functions.php
@@ -94,6 +94,8 @@ add_action( 'widgets_init', 'wptheme_rg_widgets_init' );
 function wptheme_rg_scripts() {
 	wp_enqueue_style( 'wptheme-rg-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'wptheme-rg-custom-style', get_template_directory_uri() . '/css/style.css' );
+
 	wp_enqueue_script( 'wptheme-rg-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'wptheme-rg-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
